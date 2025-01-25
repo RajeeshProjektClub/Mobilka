cc575d1b9110d45fa9690c3944c5bb598268bb40
Matikor <matikor.fanmail@gmail.com>
2025-01-25 15:53:06 +0100

    fixed listener

--- flask/main1.py
+++ flask/main1.py
@@ -41,4 +41,5 @@ def get_routine():
     return jsonify(data), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=2234, debug=True)
+    #app.run(debug=True)
